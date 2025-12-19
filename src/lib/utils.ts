be71d4870e34da5fb0f82bad7275052d2cb6f661
estanexanavsem <zvfytmbf77jbs0@hotmail.com>
2025-12-19 19:10:54 +0300

    fix: handle undefined country in formatCountry, return "Unknown

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,9 @@
 import { flag, name } from "country-emoji";
 
-export function formatCountry(country: string) {
+export function formatCountry(country?: string) {
+  if (!country) {
+    return "Unknown";
+  }
+
   return `${flag(country)} ${name(country)}`;
 }
